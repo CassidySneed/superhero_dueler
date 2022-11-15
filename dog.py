dd8848cdc92f264950c9389c58d1cd651b5e49fc
CassidySneed <teamsneedman@gmail.com>
2022-11-14 16:33:39 -0800

    created a dog class

---
+++ dog.py
@@ -0,0 +1,20 @@
+class Dog: 
+    def __init__(self, name, breed): 
+        self.name = name 
+        self.breed = breed
+        print("dog initalized!")
+
+    def bark(self): 
+        print("Woof!")
+
+    def sit(self): 
+        print(f'{self.name} sits')
+
+
+
+
+
+
+
+
+
---
+++ my_dogs.py
@@ -0,0 +1,8 @@
+import dog 
+
+lovey = dog.Dog('lovey', 'jack russel')
+my_other_dog = dog.Dog("Annie", "SuperDog")
+
+# lovey.bark()
+print(my_other_dog.name)
+lovey.sit()
